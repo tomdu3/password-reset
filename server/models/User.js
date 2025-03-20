59e2db4ba768944c00529a99759ea6888c13bd10
Tomislav Dukez <tomdu3@ymail.com>
2025-03-20 19:20:35 +0000

    add timestamps to User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
+// User Model
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  resetToken: String,
-  resetTokenExpiry: Date
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    resetToken: String,
+    resetTokenExpiry: Date
+}, {
+    timestamps: true
 });
 
 module.exports = mongoose.model('User', userSchema);
