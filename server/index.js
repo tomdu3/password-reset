1f3233d6c9b2de08ab3bb39f870f98310cd3c8d6
Tomislav Dukez <tomdu3@ymail.com>
2025-03-20 19:23:22 +0000

    add db connection to server

--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
+const connectDB = require('./config/db');
 
 const app = express();
-app.use(express.json());
-app.use(cors());
 
-// test route
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
-}
-);
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+// Routes
+app.use('/api/users', () => console.log('Users route'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
