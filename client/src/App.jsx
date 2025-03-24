659e11a39408a867ec3043352d3779ce3e09d8c8
Tomislav Dukez <tomdu3@ymail.com>
2025-03-24 12:03:07 +0000

    update App and styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ForgotPassword from './components/ForgotPassword';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
 import Login from './components/Login';
+import ForgotPassword from './components/ForgotPassword';
 import ResetPassword from './components/ResetPassword';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './App.css';
 
 const App = () => {
-    return (
-        <Router> 
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/forgot-password" element={<ForgotPassword />} />
-                <Route path="/reset-password/:token" element={<ResetPassword />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Header />
+
+      <main id="main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
