148a265d1b848852c6925bcae00f1df38b0bdf0e
Tomislav Dukez <tomdu3@ymail.com>
2025-03-24 12:03:50 +0000

    add Fotter component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer id="footer" className="footer py-3">
+      <div className="container">
+        <div className="copyright">
+          &copy; Copyright <strong><span>TomDcoding</span></strong>. All Rights Reserved
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
